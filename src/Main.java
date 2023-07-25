a454db3cb6fa7fe59350f4ebbe704a949b8c5a7e
Onespops <121386348+Onespops@users.noreply.github.com>
2023-07-25 21:44:39 +0300

    Test

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,34 @@ public class Main {
         System.out.print("Введи арифметическое выражение: ");
         String input = scanner.nextLine();
 
-        String result = calc(input);
-        System.out.println(result);
+        try {
+            String result = calc(input);
+            System.out.println("Результат: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
     }
 
     public static String calc(String input) {
-        String[] parts = input.split(" ");
 
+        String[] parts = input.split(" ");
         if (parts.length != 3) {
-            System.out.println("Cтрока не является математической операцией");
+            throw new IllegalArgumentException("Cтрока не является математической операцией");
         }
 
-        int a = Integer.parseInt(parts[0]);
-        int b = Integer.parseInt(parts[2]);
-        String operator = parts[1];
+        int a, b;
+        try {
+            a = Integer.parseInt(parts[0]);
+            b = Integer.parseInt(parts[2]);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Введены некорректные числа");
+        }
 
-        if (a <= 1 || a >= 10 || b <= 1 || b >= 10) {
-            try {
-                throw new Exception();
-            } catch (Exception e) {
-                System.out.println("Числа не входят в диапазон от 1 до 10");
-            }
+        if (a < 1 || a > 10 || b < 1 || b > 10) {
+            throw new IllegalArgumentException("Числа не входят в диапазон от 1 до 10");
         }
         int result;
+        String operator = parts[1];
         switch (operator) {
             case "+":
                 result = a + b;
@@ -40,14 +45,14 @@ public class Main {
                 result = a * b;
                 break;
             case "/":
-                if (b == 0) {
-                    throw new ArithmeticException("Деление на ноль недопустимо");
-                }
+//                if (b == 0) {
+//                    throw new ArithmeticException("Деление на ноль недопустимо");
+//                }
                 result = a / b;
                 break;
 
             default:
-                throw new IllegalStateException("Недопустимая операция");
+                throw new IllegalArgumentException("Недопустимая операция");
         }
         return String.valueOf(result);
     }
